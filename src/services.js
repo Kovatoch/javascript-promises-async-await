c2e22028d89bd68e6187a8ae9b955fd563261255
Kovatoch <38441328+Kovatoch@users.noreply.github.com>
2022-04-24 17:48:05 +0200

    Completed module

--- src/services.js
+++ src/services.js
@@ -9,20 +9,33 @@ export function fetchWithTimeout(delay) {
       .catch(error => console.log(error));
   }
   
- export async function asyncFetchMovies() {
-   try {
-    const response = await fetch("/data/movies.json");
-    const results = await response.json();
-    console.log(results);
-   }
-   catch(error) {
-      console.log(error);
-   }
- }
+
 
   export function fetchBooks() {
     return fetch("./data/books.json")
       .then(response => response.json())
       .then(books => books)
       .catch(error => console.log(error));
+  }
+
+  export async function asyncFetchMovies() {
+    try {
+     const response = await fetch("/data/movies.json");
+     const results = await response.json();
+     console.log(results);
+    }
+    catch(error) {
+       console.log(error);
+    }
+  }
+
+  export async function asyncFetchBooks() {
+    try {
+     const response = await fetch("/data/books.json");
+     const results = await response.json();
+     console.log(results);
+    }
+    catch(error) {
+       console.log(error);
+    }
   }
